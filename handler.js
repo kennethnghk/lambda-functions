fd4b1e87c2df90d6c9505f1ad2833562729f0ffb
Kenneth Ng <ng.tszcheong@gmail.com>
2018-01-27 10:10:17 +0800

    #5 Implement fetch image from url

--- handler.js
+++ handler.js
@@ -1,21 +1,35 @@
 "use strict";
 
+const qs = require("qs");
 const bunyan = require("bunyan");
-const log = bunyan.createLogger({ name: "hello" });
+const log = bunyan.createLogger({ name: "serverless-services" });
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Go Serverless v1.0! Your function executed successfully!",
-      input: event
+module.exports.fetchImage = (event, context, callback) => {
+  const { url } = qs.parse(event.body);
+
+  log.info("fetch url: " + url);
+
+  // https://github.com/serverless/examples/tree/master/aws-node-fetch-file-and-store-in-s3
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response;
+      }
+      return Promise.reject(
+        new Error(
+          `Failed to fetch ${response.url}: ${response.status} ${
+            response.statusText
+          }`
+        )
+      );
     })
-  };
-
-  log.info("Log example in hello function");
-
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+    .then(response => response.buffer())
+    .then(buffer => console.log(buffer))
+    .then(() => {
+      const response = {
+        statusCode: 200,
+        body: { url }
+      };
+      callback(null, response);
+    });
 };
